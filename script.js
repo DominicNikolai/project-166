01378aa64bf71adee0d458b896621f9bbe1e8191
DominicNikolai <d960960gm@gmail.com>
2025-06-13 20:59:04 +0200

    v0.0.5

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const $hero = $('.hero');
 const TOTAL_FRAMES = 150;
 
 function generateSrc(nFrame) {
-  return `./assets/frames/ezgif-frame-081-${nFrame}.webp`;
+  return `./assets/frames/ezgif-frame-${nFrame.toString().padStart(3,'0')}.webp`;
 }
 
 function updateImage(nFrame) {
